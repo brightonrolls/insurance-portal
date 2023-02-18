de241f32f99a71e17d900e975a4dfc0c0f8ec26f
Suraj Rajput <suraj.rajput@enquero.com>
2023-02-18 11:32:02 +0530

    Completed header section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import CssBaseline from "@mui/material/CssBaseline";
 
 import { Box } from "@mui/material";
-
-import MainMenu from "./components/MainMenu";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <CssBaseline />
       <Box sx={{ display: "block", background: "#f1f1f1" }}>
-        <MainMenu />
+        <Header />
       </Box>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,198 @@
-import React from "react";
-import MuiAppBar from "@mui/material/AppBar";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import AppBar from "@mui/material/AppBar";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
 import MenuIcon from "@mui/icons-material/Menu";
-import { styled } from "@mui/material/styles";
-import { Box, Avatar, Toolbar, IconButton, Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+
+import Logo from "../assets/images/GPTLogo.svg";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const drawerWidth = 240;
+const navItems = [
+  "My Apps",
+  "Quick Quotes",
+  "Products",
+  "Download Forms",
+  "Buy a New Policy",
+];
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+const theme = createTheme({
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          // Some CSS
+          backgroundColor: "#fff",
+          color: "#000",
+        },
+      },
+    },
+    MuiListItemText: {
+      styleOverrides: {
+        root: {
+          // Some CSS
+          color: "#000",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          // Some CSS
+          color: "#000",
+        },
+      },
+    },
+  },
+});
 
-const Header = ({ open, setOpen, handleDrawerOpen, handleDrawerClose }) => {
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <img src={Logo} alt="Logo" style={{ height: "60px" }} />
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <>
-      <AppBar position="fixed" open={open}>
-        <Box display="flex" alignItems="center" justifyContent="space-between">
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
+    <ThemeProvider theme={theme}>
+      <Box>
+        <AppBar component="nav">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{
+              mr: 2,
+              m: 4,
+              display: { sm: "none" },
+              justifyContent: "flex-start",
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box
+            sx={{
+              padding: "0 20px 0 20px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography
+              variant="h6"
+              component="div"
               sx={{
-                marginRight: 5,
-                ...(open && { display: "none" }),
+                display: { xs: "none", sm: "block" },
               }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap component="div">
-              Insurance Portal
+              <img
+                src={Logo}
+                alt="Logo"
+                style={{ height: "70px", paddingTop: "10px" }}
+              />
             </Typography>
-          </Toolbar>
-          <Box pr={3}>
-            <Avatar
-              alt="Sonu Goku"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyJbaVWMoAIfzN6APlhpLfo_-XWLaI0-CXCw&usqp=CAU"
-            />
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item) => (
+                <Button key={item}>{item}</Button>
+              ))}
+            </Box>
+            <Box
+              alignItems="center"
+              justifyContent="space-between"
+              sx={{ display: { xs: "none", sm: "flex", gap: "10px" } }}
+            >
+              <Box>
+                <Typography variant="subtitle2" color="text.secondary">
+                  Welcome Son, Goku
+                </Typography>
+                <Typography
+                  variant="subtitle2"
+                  color="text.secondary"
+                  align="right"
+                >
+                  Customer
+                </Typography>
+              </Box>
+              <Divider
+                orientation="vertical"
+                flexItem
+                sx={{
+                  backgroundColor: "#073262",
+                  width: "3px",
+                  borderRadius: "3px",
+                }}
+              />
+              <Avatar
+                alt="Sonu Goku"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyJbaVWMoAIfzN6APlhpLfo_-XWLaI0-CXCw&usqp=CAU"
+              />
+            </Box>
           </Box>
+        </AppBar>
+        {/* Drawer */}
+        <Box component="nav">
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
         </Box>
-      </AppBar>
-    </>
+      </Box>
+    </ThemeProvider>
   );
+}
+
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
 
-export default Header;
+export default DrawerAppBar;
